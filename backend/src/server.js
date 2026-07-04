30d8bf645032c0bb9613068157ce5b4a555f7caf
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2026-07-04 14:52:31 +0300

    create project

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {ENV} from "./lib/env.js";
+
+const app = express();
+console.log(ENV.PORT);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "success from backend 123" });
+});
+app.listen(ENV.PORT, () => {
+  console.log("server is running on port 3000");
+});
