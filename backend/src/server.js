d42d738a917fc0d82dd28b37e0d4e441e12d0a66
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2026-07-16 13:09:26 +0300

    setup database and dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {ENV} from "./lib/env.js";
+import {connectDB} from "./lib/db.js";
 
 const app = express();
 console.log(ENV.PORT);
@@ -7,6 +8,18 @@ console.log(ENV.PORT);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "success from backend 123" });
 });
-app.listen(ENV.PORT, () => {
-  console.log("server is running on port 3000");
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("server is running on port:", ENV.PORT);
+      connectDB();
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal" redirectUrl="/" />
+          <SignUpButton mode="modal" redirectUrl="/" />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
