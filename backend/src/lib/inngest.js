df2fe68e65e233d936dc39950e0b0621a0b4861d
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2026-07-16 18:32:10 +0300

    fix inngest problem

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -20,7 +20,7 @@ const syncUser = inngest.createFunction(
         
     }
 );
-const dleteUserFromDB = inngest.createFunction(
+const deleteUserFromDB = inngest.createFunction(
     { id: "delete-user-from-db", event: "clerk/user.deleted" },
     async ({ event }) => {
         await connectDB();
